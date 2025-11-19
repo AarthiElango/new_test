c56fe33b2df1b384442c404c94b7c6d3909b33f5
AarthiElango <142249472+AarthiElango@users.noreply.github.com>
2025-11-19 15:03:03 +0530

    added blink

--- design.v
+++ design.v
@@ -1,1 +1,13 @@
-// Enter source text here
+module blink (
+    input wire clk,
+    input wire rst,
+    output reg [3:0] led
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            led <= 4'b0001;
+        end else begin
+            led <= {led[2:0], led[3]};
+        end
+    end
+endmodule
